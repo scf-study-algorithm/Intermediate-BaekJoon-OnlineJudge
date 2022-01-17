c62acbbf6508b7a509242eaa221f3e4106804794
Kim Miso <kimms9725@naver.com>
2022-01-17 21:51:13 +0900

    22.01.17 solved.

---
+++ 5주차-백트래킹/w5-Kaya/15649.py
@@ -0,0 +1,21 @@
+import imp
+
+
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+s = []
+
+def go():
+    if len(s) == m:
+        # s 리스트 안에 있는 내용을 출력
+        print(' '.join(map(str, s)))
+        return
+    
+    for i in range(1, n+1):
+        if i not in s:
+            s.append(i)
+            go()
+            s.pop()
+
+go()
